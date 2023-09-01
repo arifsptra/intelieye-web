26ec9de1a1029f2b8e842c1ce8f1abff017ec1e9
arifsptra <arif.sptrra@gmail.com>
2023-09-01 21:16:20 +0700

    fix consume api and improve ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,102 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import React, { useState } from "react";
+import axios from "axios";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 interface FormData {
-    sentence: string;
+  sentence: string;
 }
 
 const App: React.FC = () => {
-    const [formData, setFormData] = useState<FormData> ({
-        sentence: '',
-    });
-    const [result, setResult] = useState<any>();
+  const [formData, setFormData] = useState<FormData>({
+    sentence: "",
+  });
+  const [result, setResult] = useState<any>();
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setFormData(prevData => ({ ...prevData, [name]: value }));
-    };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-        try {
-            const response = await axios.post('http://api.intelieye.my.id/', formData, {
-                headers: {
-                'Content-Type': 'application/json',
-                },
-            });
-            console.log('Form submitted successfully');
-            setResult(response.data.result);
-        } catch (error) {
-            console.error('Error submitting form:', error);
+    try {
+      const response = await axios.post(
+        "http://api.intelieye.my.id/predicts",
+        formData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-    };
+      );
+      console.log("Form submitted successfully");
+      setResult(response.data.result);
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  };
 
-    return (
-        <>
-            <Navbar />
-            <div className='w-full py-16 text-white px-4'>
-        <div className='max-w-[1240px] mx-auto grid lg:grid-cols-2'>
-            <div className='lg:col-span-2 my-4'>
-            <center>
-                <h1 className='md:text-3xl sm:text-2xl text-xl font-bold py-2'>
-                    Menyingkap Tirai Mitos dan Fakta <br /> Tersembunyi seputar Kesehatan Mata
-                </h1>
-            <p className='text-lg'>Apakah benar atau sekadar khayalan?</p>
-            </center>
-            </div>
-            <div className='my-4'>
-            
-                <form onSubmit={handleSubmit} className='flex flex-col sm:flex-row items-center justify-center w-full'>
-                    <input className='p-3 flex w-full rounded-md text-black'
-                type='text'
+  return (
+    <>
+      <Navbar />
+      <div className="w-full py-16 text-white px-4 bg-red">
+        <div className="max-w-[1240px] mx-auto">
+          <div className="lg:col-span-2 my-10 mx-auto text-center">
+            <h1 className="md:text-3xl sm:text-2xl text-xl font-bold py-2">
+              Menyingkap Tirai Mitos dan Fakta <br /> Tersembunyi seputar
+              Kesehatan Mata
+            </h1>
+            <p className="text-lg">Apakah benar atau sekadar khayalan?</p>
+          </div>
+          <div className="max-w-[600px] mx-auto my-10 md:my-6 lg:my-4">
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col sm:flex-row items-center justify-center"
+            >
+              <input
+                className="p-3 flex w-full rounded-md text-black"
+                type="text"
                 id="sentence"
                 name="sentence"
                 value={formData.sentence}
                 onChange={handleChange}
-                placeholder='Makan terlalu banyak bisa bikin minus?'
-                />
-                <button className='bg-[#00df9a] text-black rounded-md font-medium w-[200px] ml-4 my-6 px-6 py-3' type='submit'>
+                placeholder="Makan terlalu banyak bisa bikin minus?"
+              />
+              <button
+                className="bg-[#00df9a] text-black rounded-md font-medium w-[200px] ml-4 my-6 px-6 py-3"
+                type="submit"
+              >
                 Cari Tahu
-                </button>
-                </form>
-                <p>Jawabannya Adalah: </p>
-                {true && result}
+              </button>
+            </form>
+            <div className="max-w-[200px] mx-auto text-center">
+              <p>Jawabannya Adalah</p>
+              {result === "Fakta" ? (
+                <div className="bg-green-500 mt-2 py-2 rounded-md font-bold">
+                  {result}
+                </div>
+              ) : result === "Mitos" ? (
+                <div className="bg-red-500 mt-2 py-2 rounded-md font-bold">
+                  {result}
+                </div>
+              ) : result === null ? (
+                <div className="bg-yellow-500 mt-2 py-2 rounded-md font-bold">
+                  {result}
+                </div>
+              ) : (
+                <div className="bg-white mt-2 py-2 rounded-md font-bold">
+                  {result}
+                </div>
+              )}
             </div>
+          </div>
         </div>
-        </div>
-            <Footer />
-        </>
-    );
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,44 @@
-import React, { useState } from 'react';
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import React, { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
-    const [nav, setNav] = useState(false);
+  const [nav, setNav] = useState(false);
 
-    const handleNav = () => {
-        setNav(!nav);
-    };
+  const handleNav = () => {
+    setNav(!nav);
+  };
 
-    return (
-        <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-            <img className='w-28 cursor-pointer' src="https://i.postimg.cc/ht8PCZys/logo-intelieye.png" alt="Logo InteliEye" />
-            <ul className='hidden md:flex'>
-                <li className='p-4'>Beranda</li>
-                <li className='p-4'>Tentang Kami</li>
-            </ul>
-            <div onClick={handleNav} className='block md:hidden'>
-                {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
-            </div>
-            <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-                <img className='w-24 mt-6 ml-3 cursor-pointer' src="https://i.postimg.cc/ht8PCZys/logo-intelieye.png" alt="Logo InteliEye" />
-                <li className='p-4 border-b border-gray-600'>Beranda</li>
-                <li className='p-4'>Tentang Kami</li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+      <img
+        className="w-28 cursor-pointer"
+        src="https://i.postimg.cc/ht8PCZys/logo-intelieye.png"
+        alt="Logo InteliEye"
+      />
+      <ul className="hidden md:flex">
+        <li className="p-4">Beranda</li>
+        <li className="p-4">Tentang Kami</li>
+      </ul>
+      <div onClick={handleNav} className="block md:hidden">
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+      <ul
+        className={
+          nav
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
+            : "ease-in-out duration-500 fixed left-[-100%]"
+        }
+      >
+        <img
+          className="w-24 mt-6 ml-3 cursor-pointer"
+          src="https://i.postimg.cc/ht8PCZys/logo-intelieye.png"
+          alt="Logo InteliEye"
+        />
+        <li className="p-4 border-b border-gray-600">Beranda</li>
+        <li className="p-4">Tentang Kami</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
